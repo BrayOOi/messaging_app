983d1c2338e770fed42226ffa4e78d9f89e47b2a
Brayden Ooi <brayden.ooi@hotmail.com>
2019-10-27 01:40:23 +0800

    Test WebSocket io functionality

--- application.py
+++ application.py
@@ -1,14 +1,17 @@
 import os
 
 from flask import Flask, render_template, redirect
-# from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-# socketio = SocketIO(app)
+socketio = SocketIO(app)
 
 
 @app.route("/")
 def index():
-    # TODO Prompt for a username 
-    return render_template("index.html")
+  # TODO Prompt for a username aaa
+  return render_template("index.html")
+
+if __name__ == '__main__':
+    socketio.run(app)
